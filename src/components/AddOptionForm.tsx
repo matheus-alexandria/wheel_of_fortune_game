9e33d04d96681a0335ee6404cab0a2acbb5520d4
matheus_alexandria <maalexandria@hotmail.com>
2026-01-09 15:14:56 -0300

    refactor: more improvement to mobile usage

--- src/components/AddOptionForm.tsx
+++ src/components/AddOptionForm.tsx
@@ -25,40 +25,38 @@ export function AddOptionForm({ saveOption }: AddOptionForm) {
 
   return (
     <form
-      className="flex flex-col h-10 items-center mt-2 mb-4 justify-center gap-3"
+      className="flex h-10 items-center mt-2 mb-4 justify-center"
       onSubmit={(event) => {
         handleAddOption(event);
       }}
     >
-      <div className="flex items-center justify-center">
-        <input
-          type={"text"}
-          defaultValue={percentage}
-          value={percentage}
-          onChange={(e) => {
-            const valueAsNumber = Number(e.target.value);
-            if (!Number.isNaN(valueAsNumber)) {
-              setPercentage(valueAsNumber);
-            } else {
-              setPercentage((prev) => prev);
-            }
-          }}
-          className="w-12 p-2 mr-[2px] rounded-l-md"
-        />
-        <input
-          type={"text"}
-          placeholder={"Type here your option"}
-          value={option}
-          onChange={(e) => setOption(e.target.value)}
-          className="p-2 h-full"
-        />
-        <button
-          type="submit"
-          className="px-2 h-full bg-zinc-400 text-white rounded-r-md hover:bg-zinc-500 transition-colors"
-        >
-          <Plus size={20} weight="bold" />
-        </button>
-      </div>
+      <input
+        type={"text"}
+        defaultValue={percentage}
+        value={percentage}
+        onChange={(e) => {
+          const valueAsNumber = Number(e.target.value);
+          if (!Number.isNaN(valueAsNumber)) {
+            setPercentage(valueAsNumber);
+          } else {
+            setPercentage((prev) => prev);
+          }
+        }}
+        className="w-12 p-2 mr-[2px] rounded-l-md"
+      />
+      <input
+        type={"text"}
+        placeholder={"Type here your option"}
+        value={option}
+        onChange={(e) => setOption(e.target.value)}
+        className="p-2 w-full h-full"
+      />
+      <button
+        type="submit"
+        className="px-2 h-full bg-zinc-400 text-white rounded-r-md hover:bg-zinc-500 transition-colors"
+      >
+        <Plus size={20} weight="bold" />
+      </button>
     </form>
   );
 }
